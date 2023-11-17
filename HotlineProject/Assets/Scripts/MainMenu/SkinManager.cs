669ab73c0ea96c0c38b2d2169928a27ae6cd4f7f
ffeIipe <felipealderete03@gmail.com>
2023-11-17 20:48:28 -0300

    SwipeCustomizerChange

--- HotlineProject/Assets/Scripts/MainMenu/SkinManager.cs
+++ HotlineProject/Assets/Scripts/MainMenu/SkinManager.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class SkinManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public static SkinManager instance;
     [SerializeField] public Sprite[] bodySprite, headSprite, leftArmSprite, rightArmSprite;
-    
+
     public int index, maxIndex;
 
     [SerializeField] private GameDataController datacontroller;
-    
-    private void Awake() {
 
-        if(instance != null)
+    private Vector2 touchStartPos;
+    private bool isSwiping = false;
+    private float swipeThreshold = 50f;
+
+    private void Awake()
+    {
+        if (instance != null)
         {
             Destroy(gameObject);
         }
@@ -26,37 +27,75 @@ public class SkinManager : MonoBehaviour
             instance = this;
         }
         DontDestroyOnLoad(this.gameObject);
-
-        
     }
+
     void Start()
     {
         index = 0;
         maxIndex = bodySprite.Length - 1;
 
         datacontroller = FindObjectOfType<GameDataController>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
+        HandleSwipeInput();
+    }
 
+    void HandleSwipeInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    touchStartPos = touch.position;
+                    isSwiping = true;
+                    break;
+
+                case TouchPhase.Moved:
+                    if (isSwiping)
+                    {
+                        float swipeDelta = touch.position.x - touchStartPos.x;
+
+                        if (Mathf.Abs(swipeDelta) > swipeThreshold)
+                        {
+                            if (swipeDelta > 0)
+                            {
+                                LeftSwitch();
+                            }
+                            else
+                            {
+                                RigthSwitch();
+                            }
+
+                            isSwiping = false;
+                        }
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    isSwiping = false;
+                    break;
+            }
+        }
     }
 
     public void BuySkin()
     {
-        if(datacontroller.newScoreTokens >= 2)
+        if (datacontroller.newScoreTokens >= 2)
         {
             datacontroller.newScoreTokens = datacontroller.newScoreTokens - 2;
             datacontroller.newUnlockedSkins[index] = true;
             datacontroller.SaveData();
         }
-        
     }
+
     public void RigthSwitch()
     {
-        if(index == maxIndex)
+        if (index == maxIndex)
         {
             index = 0;
         }
@@ -68,7 +107,7 @@ public class SkinManager : MonoBehaviour
 
     public void LeftSwitch()
     {
-        if(index == 0)
+        if (index == 0)
         {
             index = maxIndex;
         }
@@ -80,7 +119,7 @@ public class SkinManager : MonoBehaviour
 
     public void SelectSkins()
     {
-        if(!datacontroller.newUnlockedSkins[index])
+        if (!datacontroller.newUnlockedSkins[index])
         {
             index = 0;
         }
