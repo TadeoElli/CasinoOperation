771c23cd3517fd39cd899f9d816dc75666a426bb
AlainDeleau <136400785+AlainDeleau1@users.noreply.github.com>
2023-12-10 12:45:26 -0300

    cambios esteticos tutorial

--- HotlineProject/Assets/Scripts/PauseMenu/TutorialHud.cs
+++ HotlineProject/Assets/Scripts/PauseMenu/TutorialHud.cs
@@ -23,58 +23,58 @@ public class TutorialHud : MonoBehaviour
         switch(tutorialStage)
         {
             case 1:
-                allInstructions[0].SetActive(true);
+                allInstructions[0].SetActive(true); // TapToMove
                 if(player.agent.hasPath)
                 {
                     tutorialStage = 2;
                 }
                 break;
             case 2:
-                allInstructions[0].SetActive(false);
-                allInstructions[1].SetActive(true);
-                allInstructions[2].SetActive(true);
+                allInstructions[0].SetActive(false); // TapToMove
+                allInstructions[1].SetActive(true);  // TapToMoveComplete
+                allInstructions[2].SetActive(true); //Find 2 Cards
                 if((Vector3.Distance(player.transform.position,card1.transform.position) < 10 || (Vector3.Distance(player.transform.position,card2.transform.position) < 10)))
                 {
                     tutorialStage = 3;
                 }
                 break;
             case 3:
-                allInstructions[2].SetActive(false);
-                allInstructions[3].SetActive(true);
-                allInstructions[4].SetActive(true);
-                allInstructions[7].SetActive(true);
+                allInstructions[2].SetActive(false); //Find 2 Cards
+                allInstructions[3].SetActive(true);  //Find 2 Cards Complete
+                allInstructions[4].SetActive(true); // Collect 2 Cards
+                allInstructions[7].SetActive(true); // Use Skills
                 if(GameManager.Instance.cardsInLevel == 0)
                 {
                     tutorialStage = 4;
                 }
                 break;
             case 4:
-                allInstructions[4].SetActive(false);
-                allInstructions[5].SetActive(true);
-                allInstructions[9].SetActive(true);
+                allInstructions[4].SetActive(false); // Collect 2 Cards
+                allInstructions[5].SetActive(true);  // Collect 2 Cards Complete
+                allInstructions[9].SetActive(true);  // Go to de poker table
                 if(GameManager.Instance.midGoal == null)
                 {
                     tutorialStage = 5;
                 }
                 break;
             case 5:
-                allInstructions[9].SetActive(false);
-                allInstructions[10].SetActive(true);
-                allInstructions[11].SetActive(true);
+                allInstructions[9].SetActive(false); // Go to de poker table
+                allInstructions[10].SetActive(true); // Go to de poker table complete
+                allInstructions[11].SetActive(true); // Escape
                 break;
             default:
                 break;
         }
         if((allInstructions[4].activeSelf || allInstructions[5].activeSelf ) && enemy.isAlert)
         {
-            allInstructions[4].SetActive(false);
-            allInstructions[5].SetActive(false);
-            allInstructions[6].SetActive(true);
+            allInstructions[4].SetActive(false); // Collect 2 Cards
+            allInstructions[5].SetActive(false); // Collect 2 Cards Complete
+            allInstructions[6].SetActive(true);  // Collect 2 cars failed
         }
         if(allInstructions[7].activeSelf && player._model.hasThrow)
         {
-            allInstructions[7].SetActive(false);
-            allInstructions[8].SetActive(true);
+            allInstructions[7].SetActive(false);  // Use Skills
+            allInstructions[8].SetActive(true);   // use skill complete
         }
     }
 }
